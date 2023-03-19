6c3e920e1a57d49048a2ac7dea1a3d8611040ce2
ANNVIN VINCENT <annvinvincent@gmail.com>
2023-03-19 23:51:24 +0530

    my first commit

---
+++ cypress/e2e/example.cy.js
@@ -0,0 +1,9 @@
+describe('My First Test', function () {
+    it('Navigates to google.com and verify title', function () { 
+       cy.visit('https://www.google.com'); 
+       cy.title().should('eq', 'Google'); 
+       cy.get('.gLFyf').type('cypress')
+       cy.get('.FPdoLc > center > .gNO89b').click({force:true})
+
+    }) 
+})
